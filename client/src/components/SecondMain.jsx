49b097b7a708d15a321cfb3a6b14feb6f7041d72
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-03 18:56:32 +0300

    Added test query with fetch()

--- client/src/components/SecondMain.jsx
+++ client/src/components/SecondMain.jsx
@@ -3,8 +3,23 @@ import leftArrow from '../assets/left_arrow_button.png'
 import rightArrow from '../assets/right_arrow_button.png'
 import movieCard from '../assets/movie_card.png'
 import emptyStar from '../assets/empty_star.png'
+import { useEffect, useState } from 'react'
 
 const SecondMain = ({sectionName}) => {
+    let [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=e4a864389e1a88f97675e63b530b64c7')
+            let result = await response.json()
+            setMovies(result.results)
+            console.log(result.results)
+        }
+
+        fetchData()
+        
+    }, [])
+
     return (
         <div className={styles['second-main']}>
             <section className={styles['one-section']}>
@@ -14,6 +29,11 @@ const SecondMain = ({sectionName}) => {
                         <a href="#"><img src={leftArrow} alt="left-arrow"/></a>
                         <a href="#"><img src={rightArrow} alt="right-arrow"/></a>
                     </div>
+                </div>
+                <div>
+                    {movies.map((movie) => (
+                        <p key={movie.id}>{movie.original_title}</p>
+                    ))}
                 </div>
                 <div className={styles['cards']}>
                     <div className={styles['card']}>
