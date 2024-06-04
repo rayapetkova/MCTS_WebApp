6d0fd04d474037a0910e37ff2cd2e038b701d765
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-04 21:17:30 +0300

    Added different data functions for fetching data

--- client/src/api_data/data_functions.js
+++ client/src/api_data/data_functions.js
@@ -1,5 +1,33 @@
-export default async function featuredToday() {
+export default async function getFeaturedToday() {
     let response = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=e4a864389e1a88f97675e63b530b64c7')
+    let result = await response.json()
+
+    return result.results
+}
+
+export async function getFeaturedThisWeek() {
+    let response = await fetch('https://api.themoviedb.org/3/trending/movie/week?api_key=e4a864389e1a88f97675e63b530b64c7')
+    let result = await response.json()
+
+    return result.results
+}
+
+export async function getTopRatedMovies() {
+    let response = await fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=e4a864389e1a88f97675e63b530b64c7')
+    let result = await response.json()
+
+    return result.results
+}
+
+export async function getComingSoonMovies() {
+    let response = await fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=e4a864389e1a88f97675e63b530b64c7')
+    let result = await response.json()
+
+    return result.results
+}
+
+export async function getPopularCelebrities() {
+    let response = await fetch('https://api.themoviedb.org/3/person/popular?api_key=e4a864389e1a88f97675e63b530b64c7')
     let result = await response.json()
 
     return result.results
