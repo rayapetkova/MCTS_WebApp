23fe0a0bfa9b3cdf33ea0e21d770db9046dfc80e
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-05 07:53:34 +0300

    Update Footer component - JSX and CSS

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import styles from './Footer.module.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <ul>
+                    <li>Email: r*****a1***1@gmail.com</li>
+                    <li>Call: 0899-899-899</li>
+                </ul>
+            </section>
+            <section>
+                <ul>
+                    <li><a href="#">Featured Today &#8599;</a></li>
+                    <li><a href="#">Watchlist &#8599;</a></li>
+                    <li><a href="#">Top rated &#8599;</a></li>
+                    <li><a href="#">Coming soon &#8599;</a></li>
+                    <li><a href="#">Celebrities &#8599;</a></li>
+                </ul>
+            </section>
+            <section>
+                <ul>
+                    <li><a href="#">Featured Today &#8599;</a></li>
+                    <li><a href="#">Watchlist &#8599;</a></li>
+                    <li><a href="#">Top rated &#8599;</a></li>
+                    <li><a href="#">Coming soon &#8599;</a></li>
+                    <li><a href="#">Celebrities &#8599;</a></li>
+                </ul>
+            </section>
+
+        </footer>
+    )
+}
+
+export default Footer
