f5efbf5b616ead7a2ce530b3e68834b555b99685
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-06 16:58:39 +0300

    Playing Now - get data from API

--- client/src/api_data/data_functions.js
+++ client/src/api_data/data_functions.js
@@ -32,3 +32,10 @@ export async function getPopularCelebrities() {
 
     return result.results
 }
+
+export async function getNowPlayingInTheatres() {
+    let response = await fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e4a864389e1a88f97675e63b530b64c7')
+    let result = await response.json()
+
+    return result.results
+}
