8106e76d56fc8a9327c17fccf4ddb5cfb77cc4d5
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-12 12:45:11 +0300

    Added reviews section

--- client/src/components/MovieInfo.jsx
+++ client/src/components/MovieInfo.jsx
@@ -1,7 +1,9 @@
 import styles from "./MovieInfo.module.css"
+
 import FirstSectionMovieInfo from "./firstSectionMovieInfo/FirstSectionMovieInfo";
 import SecondMainPhotos from "./secondMainPhotos/SecondMainPhotos"
 import SecondMainCelebs from "./secondMainCelebs/SecondMainCelebs"
+import Reviews from "./reviews/Reviews";
 
 const MovieInfo = () => {
 
@@ -14,38 +16,7 @@ const MovieInfo = () => {
 
                 <SecondMainCelebs />
 
-                <div className="reviews">
-                    <div className="title">
-                        <div className="left">
-                            <h2>User Reviews</h2>
-                            <a href="#">See all 1.4K</a>
-                        </div>
-
-                        <a href="#">Review +</a>
-                    </div>
-
-                    <div className="review-boxes">
-                        <div className="box">
-                            <p><span>10</span>/10</p>
-                            <h6>One of the Greatest Sequel Ever Made, Dune: Part Two Was Easily The Best Films Of The Year So Far</h6>
-                            <section className="post-info">
-                                <p>Raya Petkova</p>
-                                <p>&nbsp;   &#xb7;  20 Feb 2024</p>
-                            </section>
-                            <p className="review-desc">In the quiet embrace of ink and page, a story unfolded, timeless and sage, through the lens of a filmmaker's artistry, its essence soared, a masterpiece for all to see, i think Denis Villeneuve has just made the most visually stunning epic story of a movie that's ever been made, the most powerful story of a movie ever been told in the last 20 years, there has been no movies with this scale resulting in not just a piece of a film no more but a piece of art, it's what Infinity War and Endgame looks like...</p>
-                        </div>
-
-                        <div className="box">
-                            <p><span>10</span>/10</p>
-                            <h6>One of the Greatest Sequel Ever Made, Dune: Part Two Was Easily The Best Films Of The Year So Far</h6>
-                            <section className="post-info">
-                                <p>Raya Petkova</p>
-                                <p>&nbsp;   &#xb7;  20 Feb 2024</p>
-                            </section>
-                            <p className="review-desc">In the quiet embrace of ink and page, a story unfolded, timeless and sage, through the lens of a filmmaker's artistry, its essence soared, a masterpiece for all to see, i think Denis Villeneuve has just made the most visually stunning epic story of a movie that's ever been made, the most powerful story of a movie ever been told in the last 20 years, there has been no movies with this scale resulting in not just a piece of a film no more but a piece of art, it's what Infinity War and Endgame looks like...</p>
-                        </div>
-                    </div>
-                </div>
+                <Reviews />
             </div>
 
             <aside>
---
+++ client/src/components/reviews/Reviews.jsx
@@ -0,0 +1,41 @@
+import styles from "./Reviews.module.css"
+
+const Reviews = () => {
+
+    return (
+        <div className={styles['reviews']}>
+            <div className={styles['title']}>
+                <div className={styles['left']}>
+                    <h2>User Reviews</h2>
+                    <a href="#">See all 1.4K</a>
+                </div>
+
+                <a href="#">Review +</a>
+            </div>
+
+            <div className={styles['review-boxes']}>
+                <div className={styles['box']}>
+                    <p><span>10</span>/10</p>
+                    <h6>One of the Greatest Sequel Ever Made, Dune: Part Two Was Easily The Best Films Of The Year So Far</h6>
+                    <section className={styles['post-info']}>
+                        <p>Raya Petkova</p>
+                        <p>&nbsp;   &#xb7;  20 Feb 2024</p>
+                    </section>
+                    <p className={styles['review-desc']}>In the quiet embrace of ink and page, a story unfolded, timeless and sage, through the lens of a filmmaker's artistry, its essence soared, a masterpiece for all to see, i think Denis Villeneuve has just made the most visually stunning epic story of a movie that's ever been made, the most powerful story of a movie ever been told in the last 20 years, there has been no movies with this scale resulting in not just a piece of a film no more but a piece of art, it's what Infinity War and Endgame looks like...</p>
+                </div>
+
+                <div className={styles['box']}>
+                    <p><span>10</span>/10</p>
+                    <h6>One of the Greatest Sequel Ever Made, Dune: Part Two Was Easily The Best Films Of The Year So Far</h6>
+                    <section className={styles['post-info']}>
+                        <p>Raya Petkova</p>
+                        <p>&nbsp;   &#xb7;  20 Feb 2024</p>
+                    </section>
+                    <p className={styles['review-desc']}>In the quiet embrace of ink and page, a story unfolded, timeless and sage, through the lens of a filmmaker's artistry, its essence soared, a masterpiece for all to see, i think Denis Villeneuve has just made the most visually stunning epic story of a movie that's ever been made, the most powerful story of a movie ever been told in the last 20 years, there has been no movies with this scale resulting in not just a piece of a film no more but a piece of art, it's what Infinity War and Endgame looks like...</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Reviews;
