3626adc6a84bee43f7e166db74779d76da0ddf01
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-13 17:43:30 +0300

    First section in movie info rework

--- client/src/components/firstSectionMovieInfo/FirstSectionMovieInfo.jsx
+++ client/src/components/firstSectionMovieInfo/FirstSectionMovieInfo.jsx
@@ -2,30 +2,51 @@ import styles from "../firstSectionMovieInfo/FirstSectionMovieInfo.module.css"
 
 import movieImg from '../../assets/movie_img.png'
 import moviePoster from '../../assets/movie_poster.png'
+import { useDebugValue, useEffect, useState } from "react"
+import { getMovieCredits, getMovieInfo } from "../../api_data/dataFunctions"
+import { extractMovieGenres, extractDirectors } from "../../api_data/extractingData"
 
-const FirstSectionMovieInfo = () => {
+const pathForImages = 'https://image.tmdb.org/t/p/w500'
+
+const FirstSectionMovieInfo = ({ movieId }) => {
+    let [movieInfo, setMovieInfo] = useState({})
+    let [movieCredits, setMovieCredits] = useState({})
+
+    useEffect(() => {
+        async function loadMovieInfo() {
+            let movieData = await getMovieInfo(movieId)
+            setMovieInfo(movieData)
+        }
+
+        async function loadMovieCredits() {
+            let movieCastAndCrew = await getMovieCredits(movieId)
+            setMovieCredits(movieCastAndCrew)
+        }
+
+        loadMovieInfo()
+        loadMovieCredits()
+    }, [])
 
     return (
         <section className={styles['first-section']}>
-            <h1>Dune: Part Two</h1>
+            <h1>{movieInfo.original_title}</h1>
             <div className={styles['small-info']}>
                 <ul className={styles['left-ul']}>
-                    <li>2024</li>
-                    <li>&nbsp;   &#xb7;  EN</li>
-                    <li>&nbsp;   &#xb7;  2h 46m</li>
+                    <li>{movieInfo.release_date}</li>
+                    <li>&nbsp;   &#xb7;  {movieInfo.original_language ? movieInfo.original_language.toUpperCase() : movieInfo.original_language}</li>
+                    <li>&nbsp;   &#xb7;  {movieInfo.runtime} min</li>
                 </ul>
 
                 <div className={styles['buttons']}>
                     <a href="#">Review</a>
-                    <a href="#">8.9/10</a>
                 </div>
             </div>
             <div className={styles['two-images']}>
                 <div className={`${styles['img-container']} ${styles['first']}`}>
-                    <img src={moviePoster} alt="movie-poster" />
+                    <img src={`${pathForImages}/${movieInfo.poster_path}`} alt="movie-poster" />
                 </div>
                 <div className={`${styles['img-container']} ${styles['second']}`}>
-                    <img src={movieImg} alt="movie-image" />
+                    <img src={`${pathForImages}/${movieInfo.backdrop_path}`} alt="movie-image" />
                 </div>
             </div>
             <div className={styles['movie-info']}>
@@ -33,17 +54,17 @@ const FirstSectionMovieInfo = () => {
                     <tbody>
                         <tr>
                             <td>Genre</td>
-                            <td>Adventure, Action, Drama</td>
+                            <td>{movieInfo.genres ? extractMovieGenres(movieInfo.genres) : "Loading..."}</td>
                         </tr>
 
                         <tr>
                             <td>Plot</td>
-                            <td>Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.</td>
+                            <td>{movieInfo.overview}</td>
                         </tr>
 
                         <tr>
                             <td>Director</td>
-                            <td>Denis Villenueve</td>
+                            <td>{movieCredits ? extractDirectors(movieCredits.crew) : "Loading..."}</td>
                         </tr>
 
                         <tr>
