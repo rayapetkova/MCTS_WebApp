d611b241f23d6edc823c9bd872764b8d12678235
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-06-15 21:10:15 +0300

    Changed "a" tags to "Link" and added paths; some CSS changes

--- client/src/components/firstMain/FirstMain.jsx
+++ client/src/components/firstMain/FirstMain.jsx
@@ -6,17 +6,20 @@ import yellowDot from '../../assets/yellow_dot.png'
 import thirdImg from '../../assets/third.png'
 import { useEffect, useState } from 'react'
 import { getNowPlayingInTheatres } from '../../api_data/dataFunctions'
+import { Link } from 'react-router-dom'
 
 const pathForImages = 'https://image.tmdb.org/t/p/w500'
 
 const FirstMain = () => {
     let [playingNow, setPlayingNow] = useState([])
+    let [firstMovie, setFirstMovie] = useState({})
 
     useEffect(() => {
         async function loadPlayingNow() {
             let playingNowInTheatres = await getNowPlayingInTheatres()
 
             setPlayingNow(playingNowInTheatres)
+            setFirstMovie(playingNowInTheatres[0])
         }
         
         loadPlayingNow()
@@ -27,16 +30,18 @@ const FirstMain = () => {
             <section className={styles['first']}>
                 <div className={styles['left']}>
                     <div className={styles['image-container']}>
-                        <img src={firstImg} alt="first-img" className={styles['first-img']}/>
+                        <img src={`${pathForImages + firstMovie.backdrop_path}`} alt="first-img" className={styles['first-img']}/>
                     </div>
                     <div className={styles['play-info']}>
-                        <img src={playButton} alt="play_button"/>
+                        <Link to={`/movies/${firstMovie.id}/details`}><img src={playButton} alt="play_button"/></Link>
                         <section className={styles['title']}>
-                            <h2>Inside Out 2 Make us Feel Every Emotion</h2>
-                            <h4>See more information</h4>
+                            <h2>{firstMovie.title}</h2>
+                            <Link to={`/movies/${firstMovie.id}/details`}>See more information</Link>
                         </section>
                     </div>
-                    <img src={secondImg} alt="second-img" className={styles['second-img']}/>
+                    <Link to={`/movies/${firstMovie.id}/details`}>
+                        <img src={`${pathForImages + firstMovie.poster_path}`} alt="second-img" className={styles['second-img']}/>
+                    </Link>
                 </div>
 
                 <div className={styles['right']}>
@@ -47,10 +52,10 @@ const FirstMain = () => {
                         </div>
                         <a href="#">Browse Movies &gt;</a>
                     </div>
-                    {playingNow.slice(0, 3).map((movie) => (
-                        <div key={movie.id} className={styles['featured-images']}>
+                    {playingNow.slice(1, 4).map((movie) => (
+                        <Link to={`/movies/${movie.id}/details`} key={movie.id} className={styles['featured-images']}>
                             <img src={`${pathForImages + movie.backdrop_path}`} alt="featured-movie"/>
-                        </div>
+                        </Link>
                     ))}
                 </div>
             </section>
