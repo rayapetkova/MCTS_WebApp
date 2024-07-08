54ba742cee32b895f1b9a604c3512fabd2b81233
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-07-08 17:39:02 +0300

    HTML and CSS rework

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -13,13 +13,12 @@ import {
 
 import styles from './Login.module.css'
 
-
-
 import useForm from '../../hooks/useForm';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext';
 
 import logoSymbol from "../../assets/logo_without_background.png"
+import { Link } from 'react-router-dom'
 
 const formNames = {
     email: 'email',
@@ -43,7 +42,7 @@ const Login = () => {
                         <MDBCol md='10' lg='6' className={`order-2 order-lg-1 d-flex flex-column align-items-center ${styles['left']}`}>
                             <form onSubmit={onSubmit}>
 
-                                <p className={`text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 ${styles['title']}`}>Sign Up</p>
+                                <p className={`text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4 ${styles['title']}`}>Log In</p>
 
                                 <div className="d-flex flex-row align-items-center mb-4">
                                     <MDBInput 
@@ -70,12 +69,12 @@ const Login = () => {
                                 </div>
 
                                 <div className='mb-4'>
-                                    <MDBRow name='flexCheck' id='flexCheckDefault'>
-                                        
+                                    <MDBRow name='flexCheck' id='flexCheckDefault' className={styles['additional-link']}>
+                                        <p>Don't have an account?</p><Link to={'/register'}>Register</Link>
                                     </MDBRow>
                                 </div>
 
-                                <MDBBtn className='mb-4' size='lg'>Register</MDBBtn>
+                                <MDBBtn className='mb-4' size='lg'>Log In</MDBBtn>
                             </form>
                         </MDBCol>
 
