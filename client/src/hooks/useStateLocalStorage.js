e340f99e581c7b7558e473285f73b275633aebb2
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-07-16 17:13:18 +0300

    Add custom hook for managing state

---
+++ client/src/hooks/useStateLocalStorage.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function useStateLocalStorage(key, initialValue) {
+    const [state, setState] = useState(() => {
+        const authData = localStorage.getItem('authData')
+
+        if (authData) {
+            return authData
+        }
+
+        return initialValue
+    })
+
+    const setStateFunction = (value) => {
+        setState(value)
+        localStorage.setItem(key, JSON.stringify(value))
+    }
+
+    return [
+        state,
+        setStateFunction
+    ]
+}
+
+export default useStateLocalStorage;
