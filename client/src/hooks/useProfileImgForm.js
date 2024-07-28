366f8afb8c070f8874020538aa8aaf1a24a6fc8d
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-07-28 23:06:36 +0300

    Add custom hook for uploading profile image

---
+++ client/src/hooks/useProfileImgForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import uploadImage from "../api_data/cloudinary/uploadImage";
+
+function useProfileImgForm(submitHandler, initialValue) {
+    const [profileImg, setProfileImg] = useState(initialValue)
+
+    const onChangeImg = (e) => {
+        const uploadedImg = e.target.files[0]
+        setProfileImg(uploadedImg)
+    }
+
+    const onSubmitImg = async (e) => {
+        e.preventDefault()
+        const formData = new FormData()
+
+        formData.append('file', profileImg)
+        formData.append('upload_preset', 'uarqt4he')
+
+        const result = await uploadImage(formData)
+
+        submitHandler(result.url)
+    }
+
+    return [
+        onChangeImg,
+        onSubmitImg
+    ]
+}
+
+export default useProfileImgForm;
