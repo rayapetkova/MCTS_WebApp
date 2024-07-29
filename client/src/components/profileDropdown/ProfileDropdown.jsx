f21cbee2b25ef9fe1d3621d7c1d3ff04fa57b693
Raya Petkova <122923696+rayapetkova@users.noreply.github.com>
2024-07-29 14:42:24 +0300

    Add Profile Dropdown Menu

---
+++ client/src/components/profileDropdown/ProfileDropdown.jsx
@@ -0,0 +1,30 @@
+import {
+    MDBDropdown,
+    MDBDropdownMenu,
+    MDBDropdownToggle,
+    MDBDropdownItem
+} from 'mdb-react-ui-kit';
+
+import profileIcon from '../../assets/profile_icon.png'
+import styles from './ProfileDropdown.module.css'
+import { Link } from 'react-router-dom'
+
+
+const ProfileDropdown = ({ logoutSubmitHandler }) => {
+    return (
+        <MDBDropdown>
+            <MDBDropdownToggle tag='a' className={`btn btn-primary ${styles['dropdown-container']}`}>
+                <div className={styles['profile-img-container']}>
+                    <img src={profileIcon} alt="profileImg" />
+                </div>
+                
+            </MDBDropdownToggle>
+            <MDBDropdownMenu className={styles['dropdown-items']} dark>
+                <Link to={'/users/me'}><MDBDropdownItem className={styles['dropdown-item']}>Profile Details</MDBDropdownItem></Link>
+                <Link onClick={() => logoutSubmitHandler()}><MDBDropdownItem className={styles['dropdown-item']}>Log Out</MDBDropdownItem></Link>
+            </MDBDropdownMenu>
+        </MDBDropdown>
+    );
+}
+
+export default ProfileDropdown;
